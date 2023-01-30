bae1c002ad652eeda76be020845b56d5f0642153
waseem <waseem@sentientnow.com>
2023-01-30 22:21:24 +0530

    vc3

--- app/build/generated/source/buildConfig/release/com/waseem/brickgame/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/waseem/brickgame/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.waseem.brickgame";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 1;
+  public static final int VERSION_CODE = 3;
   public static final String VERSION_NAME = "1.0";
 }
